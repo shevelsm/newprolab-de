d52c28931f796995a9a55d62f1a950b4ba0d5d04
shevelsm <shevelsm@gmail.com>
2021-04-18 22:42:40 +0300

    update submit

--- lab04s/submit.sh
+++ lab04s/submit.sh
@@ -3,7 +3,12 @@
 echo $1
 PYSPARK_PYTHON=/usr/share/python3-ml/bin/python3.6 spark-submit \
     --conf spark.streaming.batch.duration=10 \
-    --master local[1] \
-    --executor-memory 4G \
+    --conf spark.executor.instances=3 \
+    --conf spark.executor.memory=4g \
+    --conf spark.driver.memory=1g \
+    --conf spark.sql.shuffle.partitions=3 \
+    --conf spark.default.parallelism=3 \
+    --master yarn \
+    --deploy-mode client\
     --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.2 \
     $1
