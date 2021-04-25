894c1d6566217675b3d9031c14af39c1fe7dcb8d
shevelsm <shevelsm@gmail.com>
2021-04-25 17:47:32 +0300

    lab05 start

---
+++ lab05/flask_app.py
@@ -0,0 +1,47 @@
+import json
+import logging
+from flask import Flask, request
+from joblib import load
+import pandas as pd
+import re
+from urllib.parse import urlparse
+from urllib.request import unquote
+
+
+def url2domain(url):
+    url = re.sub('(http(s)*://)+', 'http://', url)
+    parsed_url = urlparse(unquote(url.strip()))
+    if parsed_url.scheme not in ['http','https']: return None
+    netloc = re.search("(?:www\.)?(.*)", parsed_url.netloc).group(1)
+    if netloc is not None: return str(netloc).strip()
+    return None
+
+def list2domain(rec):
+    result = []
+    for x in rec:
+       result.append(url2domain(x["url"]))
+    return ', '.join(result)
+
+
+model = load("model_lab05.joblib")
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, world!"
+
+@app.route("/predict_gender_age", methods=["POST"])
+def predict_age():
+    js = json.loads(request.json)
+    df = pd.DataFrame(js)
+    df.visits = df.visits.apply(list2domain)
+    df["gender_age"] = model.predict(df["visits"])
+    res = ""
+    for index, row in df.iterrows():
+        res = res + '{"uid": "' + row['uid'] + '", "gender_age": "' + row['gender_age'] + '"}' + "\n"
+    logging.info(res)
+    return res
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
